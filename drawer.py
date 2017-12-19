b29de4bd49a666abdaef8f6db22b1814a62f1637
Max Rademacher <maxrademacher@yahoo.com>
2017-12-18 21:57:06 -0500

    Added pen size control

--- drawer.py
+++ drawer.py
@@ -4,30 +4,35 @@ def push_state(stack):
     def command():
         pos = turtle.position()
         heading = turtle.heading()
-        stack.append((pos, heading))
+        pensize = turtle.pensize()
+        stack.append((pos, heading, pensize))
     return command
 
 def pop_state(stack):
     def command():
-        pos, heading = stack.pop()
+        pos, heading, pensize = stack.pop()
         turtle.penup()
         turtle.goto(pos)
         turtle.setheading(heading)
+        turtle.pensize(pensize)
         turtle.pendown()
     return command
 
 class Drawer:
 
-    def __init__(self, draw_speed, init_angle, fwd_amt, turn_amt):
+    def __init__(self, draw_speed, init_angle, init_size, fwd_amt, turn_amt, inc_amt):
         turtle.speed(draw_speed)
         turtle.setheading(init_angle)
+        turtle.pensize(init_size)
         self.stack = []
         self.commands = {
             'F': lambda: turtle.forward(fwd_amt),
             '+': lambda: turtle.right(turn_amt),
             '-': lambda: turtle.left(turn_amt),
             '[': push_state(self.stack),
-            ']': pop_state(self.stack)
+            ']': pop_state(self.stack),
+            'p': lambda: turtle.pensize(max(1, turtle.pensize() - inc_amt)),
+            'P': lambda: turtle.pensize(min(100, turtle.pensize() + inc_amt))
         }
 
     def draw(self, draw_str):
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from system import System
 
 rules = {
     'X': 'F[-X][X]F[-X]+FX',
-    'F': 'FF'
+    'F': 'pFF'
 }
 
 s = System('X', rules)
-d = Drawer(0, 60, 3, 25)
+d = Drawer(draw_speed=0, init_angle=0, init_size=7, fwd_amt=5, turn_amt=30, inc_amt=0.1)
 
 d.draw(s.get_str(6))
 
